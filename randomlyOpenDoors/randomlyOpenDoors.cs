351898b0fb9f6dd84752c9074e2b761e536864e4
BuildTools <unconfigured@null.spigotmc.org>
2021-11-03 23:12:04 +0100

    - pain

--- randomlyOpenDoors/randomlyOpenDoors.cs
+++ randomlyOpenDoors/randomlyOpenDoors.cs
@@ -9,6 +9,12 @@ namespace randomlyOpenDoors
 {
     public class randomlyOpenDoors : Plugin<Config>
     {
+        public override string Author => "FreeGamer";
+        public override string Name => "randomlyOpenDoors";
+        public override string Prefix => "rOD";
+        public override Version Version => new Version(1, 0, 0);
+        public override Version RequiredExiledVersion => new Version(3, 4, 0);
+
         private static readonly Lazy<randomlyOpenDoors> LazyInstance = new Lazy<randomlyOpenDoors>(() => new randomlyOpenDoors());
         public static randomlyOpenDoors Instance => LazyInstance.Value;
 
@@ -47,10 +53,8 @@ namespace randomlyOpenDoors
 
         public static void findDoors()
         {
-            foreach (Door door in Map.Doors)
-            {
-                door.IsOpen = true;
-            }
+
         }
+
     }
 }
