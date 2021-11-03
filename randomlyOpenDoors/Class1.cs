e8da3ce9f2a3fa17d3f7c0a2764441d2db79e7c6
BuildTools <unconfigured@null.spigotmc.org>
2021-11-03 17:58:23 +0100

    Projektdateien hinzufügen.

---
+++ randomlyOpenDoors/Class1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace randomlyOpenDoors
+{
+    public class Class1
+    {
+    }
+}
